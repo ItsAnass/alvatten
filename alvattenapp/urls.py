9644435a2ef2928c896ce06ec164a9ab2eb89d78
Anas <doman@DESKTOP-F5PAK9Q>
2025-09-18 14:15:07 +0100

    Added the needed files

--- alvattenapp/urls.py
+++ alvattenapp/urls.py
@@ -5,4 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
+    path('contact/', views.ContactPageView.as_view(), name='contact'),
+    path('services/', views.ServicesPageView.as_view(), name='services'),
+    path('projects/', views.ProjectsPageView.as_view(), name='projects'),
+    path('features/', views.FeaturesPageView.as_view(), name='features'),
+    path('team/', views.TeamPageView.as_view(), name='team'),
+    path('testimonial/', views.TestimonialPageView.as_view(), name='testimonial'),
+       
 ]
--- alvattenapp/views.py
+++ alvattenapp/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "index.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+    
+class ContactPageView(TemplateView):
+    template_name = "contact.html"    
+
+class ServicesPageView(TemplateView):
+    template_name = "service.html"
+
+class ProjectsPageView(TemplateView):
+    template_name = "project.html"
+
+class FeaturesPageView(TemplateView):
+    template_name = "feature.html"
+
+class TeamPageView(TemplateView):
+    template_name = "team.html"
+
+class TestimonialPageView(TemplateView):
+    template_name = "testimonial.html"
 
